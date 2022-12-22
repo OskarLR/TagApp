98b26e4534f65001a0b8bbe1e5c8192d5517aabb
Oremv <oremvang@gmail.com>
2022-12-22 04:25:42 +0100

    minor refactoring

--- src/components/PlayerProfile.jsx
+++ src/components/PlayerProfile.jsx
@@ -5,32 +5,34 @@ function PlayerProfile(props) {
 
     let player = props.player
 
-    const secondsInADay = 86400
-    const secondsInAnHour = 3600
-    const secondsInAMinute = 60
-    
-    let tagTimeInSeconds = props.player.tagtime;
-    let days = Math.floor(tagTimeInSeconds/secondsInADay)
-    let hours = Math.floor((tagTimeInSeconds-days*secondsInADay)/secondsInAnHour)
-    let minutes = Math.floor((tagTimeInSeconds-days*secondsInADay-hours*secondsInAnHour)/secondsInAMinute)
-    let seconds = tagTimeInSeconds-days*secondsInADay-hours*secondsInAnHour-minutes*secondsInAMinute
+    function secondsToTimeFormat(tagTimeInSeconds){
+        const secondsInADay = 86400
+        const secondsInAnHour = 3600
+        const secondsInAMinute = 60
+        
+        let days = Math.floor(tagTimeInSeconds/secondsInADay)
+        let hours = Math.floor((tagTimeInSeconds-days*secondsInADay)/secondsInAnHour)
+        let minutes = Math.floor((tagTimeInSeconds-days*secondsInADay-hours*secondsInAnHour)/secondsInAMinute)
+        let seconds = tagTimeInSeconds-days*secondsInADay-hours*secondsInAnHour-minutes*secondsInAMinute
 
-    let daysString = ""
-    if(days>0) daysString = days + " days, "
+        let daysString = ""
+        if(days>0) daysString = days + " days, "
 
-    let hoursString = ""
-    if(hours>0) hoursString = hours + " hours, "
+        let hoursString = ""
+        if(hours>0) hoursString = hours + " hours, "
 
-    let minutesString = ""
-    if(minutes>0) minutesString = minutes + " minutes, "
+        let minutesString = ""
+        if(minutes>0) minutesString = minutes + " minutes, "
 
-    let timeTitle = daysString + hoursString + minutesString + seconds + " seconds"
+        return daysString + hoursString + minutesString + seconds + " seconds"
+    }
+
     return (
         <div id="playerprofile">
             <img id="profilepicture" src={profileicon} alt="Player Icon"/>
             <p id="playername">{player.isPreviousTagger && !props.isCurrentPlayer ?
                                 player.name  + "  tagged you!": player.name}</p>
-            <p id="tagtime">{timeTitle}</p>
+            <p id="tagtime">{secondsToTimeFormat(props.player.tagtime)}</p>
         </div>
     );
 }
